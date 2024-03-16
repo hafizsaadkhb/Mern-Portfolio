f5f2a79319a4584b4de2dd6ddfc1a4f4cd41e531
hafizsaadkhb <hafizsaadkhb@gmail.com>
2024-03-17 02:20:09 +0500

    Hello World

--- server/server.js
+++ server/server.js
@@ -8,16 +8,17 @@ const portfolioRoute = require("./routes/portfolioRoute");
 app.use(express.json());
 
 app.use("/api/portfolio", portfolioRoute);
+app.get('/', (req, res)=>(res.status(200).json({ "message": "Hello World!"})))
 
 const port = process.env.PORT || 5000;
 const path = require("path");
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-  })
-}
+// if(process.env.NODE_ENV === "production"){
+//   app.use(express.static(path.join(__dirname, "client/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client/build/index.html"));
+//   })
+// }
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
