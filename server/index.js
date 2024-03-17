5b8cd3d646b534cf74bd91fbdb5578e27d7cce36
hafizsaadkhb <hafizsaadkhb@gmail.com>
2024-03-18 01:39:40 +0500

    updated

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const portfolioRoute = require("./routes/portfolioRoute");
 
 app.use(express.json());
 app.use(cors({
-  origin: ["https://mern-portfolio-lemon.vercel.app/"],
+  origin: ["https://mern-portfolio-lemon.vercel.app"],
   methods: ["POST", "GET"],
   credientials: true
 }))
